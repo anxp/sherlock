ace7f5a86c3457479456971ef3b371d274303138
Andrey Kaplunenko <me@kaplunenko.name>
2019-01-07 19:59:46 +0200

    ajax request for selected markets added

--- ajax_content_request.js
+++ ajax_content_request.js
@@ -7,6 +7,17 @@
     };
     var ajax = new Drupal.ajax(false, false, settings);
     ajax.eventResponse(ajax, {});
+
+    //Request for _user_selected_ fleamarkets (we do this via ajax), and then fetch every market by getMarketOffers():
+    var basePath = Drupal.settings.basePath;
+    $.post(basePath + "sherlock/selected-markets", null, function (response) {
+      getMarketOffers(response);
+    }, "json");
+
   });
+
+  function getMarketOffers(x) {
+    console.log(x);
+  }
 
 })(jQuery);
