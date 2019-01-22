abfa603a7bc495a0c2a0e562fdee04fd64a0fc54
Andrey Kaplunenko <me@kaplunenko.name>
2019-01-22 23:57:39 +0200

    First simple working demo. Only OLX subclass with parsing method added at the moment.

--- ajax_content_request.js
+++ ajax_content_request.js
@@ -1,23 +1,37 @@
 (function($) {
 
   $(document).ready(function() {
-    var settings = {
-      url: Drupal.settings.basePath + "get_demo_content",
+    //Make ajax-request for USER SELECTED fleamarkets (then fetch every market by getMarketOffers()):
+    var request_settings = {
+      url: Drupal.settings.basePath + "sherlock/selected-markets",
       submit: {},
     };
-    var ajax = new Drupal.ajax(false, false, settings);
-    ajax.eventResponse(ajax, {});
 
-    //Request for _user_selected_ fleamarkets (we do this via ajax), and then fetch every market by getMarketOffers():
-    var basePath = Drupal.settings.basePath;
-    $.post(basePath + "sherlock/selected-markets", null, function (response) {
-      getMarketOffers(response);
-    }, "json");
+    var ajax_request = new Drupal.ajax(false, false, request_settings);
+    ajax_request.options.success = function(response, status, xmlhttprequest) {
+      console.log(typeof response);
+      for(let i = 0; i < response.length; i++){
+        console.log(response[i]);
+        getMarketOffers(response[i]);
+      }
+    };
 
+    ajax_request.eventResponse(ajax_request, {});
   });
 
-  function getMarketOffers(x) {
-    console.log(x);
+  function getMarketOffers(selected_market) {
+    var basePath = Drupal.settings.basePath;
+    var request_settings = {
+      url: Drupal.settings.basePath + "sherlock/market-fetch",
+      submit: {
+        market_id: selected_market,
+      },
+    };
+    var ajax_request = new Drupal.ajax(false, false, request_settings);
+    ajax_request.options.success = function(response, status, xmlhttprequest) {
+      console.log(response);
+    };
+    ajax_request.eventResponse(ajax_request, {});
   }
 
 })(jQuery);
---
+++ sherlock_ItemSniper.class.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 2018-12-20
+ * Time: 08:09
+ */
+
+require_once 'phpQuery/phpQuery.php';
+
+abstract class ItemSniper {
+  //Just the most popular user agent, it can be overriden by setUserAgent() method:
+  protected $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36';
+
+  //This value will be set to CURLOPT_CONNECTTIMEOUT. Can be overriden by setConnectionTimeout() method:
+  //TODO: Read about connection timeout in cURL, and set something more correct (2 sec?)
+  protected $connectionTimeout = 10;
+
+  //URL to fetch. We will set it up in constructor:
+  protected $URL = '';
+
+  //How deep (in pages) our script will dig. 0 for UNLIMITED.
+  protected $pageLimit = 0;
+
+  //How to find advertisement block in webpage - search string for phpQuery ('SP' stands for 'Search Pattern'):
+  protected $advertBlockSP = '';
+
+  //How to find title of given advertisement block. This 'path' is not absolute, but relative to $advertBlockSP ('SP' stands for 'Search Pattern'):
+  protected $titleSP = '';
+
+  //How to find title link (link to item page with full description, 'SP' stands for 'Search Pattern'):
+  protected $titleLinkSP = '';
+
+  //How to find price of given advertisement block. This 'path' is not absolute, but relative to $advertBlockSP ('SP' stands for 'Search Pattern'):
+  protected $priceSP = '';
+
+  //How to find link to next page in webpage with advertisements - search string for phpQuery ('SP' stands for 'Search Pattern'):
+  protected $nextPageLinkSP = '';
+
+  //$URL - URL to fetch;
+  //We do constructor protected, because we want create only objects of child classes.
+  protected function __construct($URL, $pageLimit, $advertBlockSP, $titleSP, $titleLinkSP, $priceSP, $nextPageLinkSP) {
+    $this->URL = $URL;
+    $this->pageLimit = $pageLimit;
+    $this->advertBlockSP = $advertBlockSP;
+    $this->titleSP = $titleSP;
+    $this->titleLinkSP = $titleLinkSP;
+    $this->priceSP = $priceSP;
+    $this->nextPageLinkSP = $nextPageLinkSP;
+  }
+
+  /**
+   * @param string $userAgent
+   */
+  public function setUserAgent(string $userAgent): void {
+    $this->userAgent = $userAgent;
+  }
+
+  /**
+   * @param int $connectionTimeout
+   */
+  public function setConnectionTimeout(int $connectionTimeout): void {
+    $this->connectionTimeout = $connectionTimeout;
+  }
+
+  /**
+   * @return string
+   */
+  public function getUserAgent(): string {
+    return $this->userAgent;
+  }
+
+  /**
+   * @return int
+   */
+  public function getConnectionTimeout(): int {
+    return $this->connectionTimeout;
+  }
+
+  /**
+   * @return string
+   * @param string $url
+   */
+  protected function getWebpageHtml($url) {
+    $ch = curl_init();
+
+    curl_setopt_array($ch, [
+      CURLOPT_USERAGENT => $this->getUserAgent(),
+      CURLOPT_CONNECTTIMEOUT => $this->getConnectionTimeout(),
+      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_HEADER => FALSE,
+      CURLOPT_FOLLOWLOCATION => TRUE, //Follow redirects
+      CURLOPT_URL => $url,
+    ]);
+
+    //Get URL content in string:
+    $htmlString = curl_exec($ch);
+
+    //Close handle to release resources
+    curl_close($ch);
+
+    return $htmlString;
+  }
+
+  /**
+   * @return array
+   */
+  abstract public function grabItems() : array; //Method for parsing. Need to be declared in child class, because realization depends on target market website
+}
+
+/*
+ * Naming convention for child classes:
+ * They must have prefix with marketId and underscore symbol, like: olx_, bsp_, skl_, with word 'ItemSniper' after prefix: ex.: olx_ItemSniper.
+ * This makes possible to create new instances automatically, knowing only marketId.
+ * */
+class olx_ItemSniper extends ItemSniper {
+
+  public function __construct($URL, $pageLimit, $advertBlockSP = 'div.offer-wrapper', $titleSP = 'h3', $titleLinkSP = 'h3 > a', $priceSP = 'p.price', $nextPageLinkSP = 'div.pager span.next a') {
+    parent::__construct($URL, $pageLimit, $advertBlockSP, $titleSP, $titleLinkSP, $priceSP, $nextPageLinkSP);
+  }
+
+  public function grabItems(): array {
+    $pgCount = 0;
+    $collectedItems = []; //Here we will store all found items. This is indexed array with associative sub-arrays.
+    $cii = 0; //cii == Collected Items Iterator.
+    $url = $this->URL;
+    do {
+      $fullHTML = $this->getWebpageHtml($url);
+      $doc = phpQuery::newDocument($fullHTML);
+      $offerBlocks = $doc->find($this->advertBlockSP);
+      foreach ($offerBlocks as $block) {
+        $pq_block = pq($block);
+        $itemTitle = $pq_block->find($this->titleSP)->text();
+
+        //We parse link to array and then gather only required parts.
+        //For example, usually links from OLX ends with smth like "#1b78919f15;promoted" and we don't need such trash.
+        $itemLink = $pq_block->find($this->titleLinkSP)->attr('href');
+        $linkParts = parse_url($itemLink);
+        $itemLinkClean = $linkParts['scheme'].'://'.$linkParts['host'].$linkParts['path']; //Link now clean.
+
+        $itemPrice = $pq_block->find($this->priceSP)->text();
+
+        $collectedItems[$cii]['title'] = $itemTitle;
+        $collectedItems[$cii]['link'] = $itemLinkClean;
+        $collectedItems[$cii]['price'] = $itemPrice;
+        $cii++;
+      }
+
+      //Get link to next search results page:
+      $nextPageLink = $doc->find($this->nextPageLinkSP)->attr('href');
+
+      phpQuery::unloadDocuments($doc);
+
+      $url = $nextPageLink;
+      $pgCount++;
+
+      //If digging limit not reached ($pgCount < $this->pageLimit),
+      //or $this->pageLimit set to 0 (Unlimited) -> set $willContinue flag to TRUE:
+      $willContinue = (($pgCount < $this->pageLimit) || ($this->pageLimit === 0));
+
+    } while (($url !== '') && $willContinue);
+
+    return $collectedItems;
+  }
+}
