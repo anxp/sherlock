2f8c72e9a886b560992f37da12b671d4ce810b59
Andrey Kaplunenko <me@kaplunenko.name>
2019-01-26 23:33:26 +0200

    frontend part for display results added (js for table generation and css for this table)

--- ajax_content_request.js
+++ ajax_content_request.js
@@ -2,16 +2,16 @@
 
   $(document).ready(function() {
     //Make ajax-request for USER SELECTED fleamarkets (then fetch every market by getMarketOffers()):
-    var request_settings = {
+    let request_settings = {
       url: Drupal.settings.basePath + "sherlock/selected-markets",
       submit: {},
     };
 
-    var ajax_request = new Drupal.ajax(false, false, request_settings);
+    let ajax_request = new Drupal.ajax(false, false, request_settings);
     ajax_request.options.success = function(response, status, xmlhttprequest) {
-      console.log(typeof response);
+      //console.log(typeof response);
       for(let i = 0; i < response.length; i++){
-        console.log(response[i]);
+        //console.log(response[i]);
         getMarketOffers(response[i]);
       }
     };
@@ -20,18 +20,65 @@
   });
 
   function getMarketOffers(selected_market) {
-    var basePath = Drupal.settings.basePath;
-    var request_settings = {
+    let basePath = Drupal.settings.basePath;
+    let request_settings = {
       url: Drupal.settings.basePath + "sherlock/market-fetch",
       submit: {
         market_id: selected_market,
       },
     };
-    var ajax_request = new Drupal.ajax(false, false, request_settings);
+    let ajax_request = new Drupal.ajax(false, false, request_settings);
     ajax_request.options.success = function(response, status, xmlhttprequest) {
-      console.log(response);
+      let currentMarketOutputBlock = document.getElementById(selected_market + '-output-block');
+      let tbl = generateTable(response);
+      currentMarketOutputBlock.appendChild(tbl);
     };
+
     ajax_request.eventResponse(ajax_request, {});
   }
 
 })(jQuery);
+
+function generateTable(tableContent) {
+  let tbl = document.createElement('table');
+  tbl.classList.add("sherlock-automatically-generated-table");
+
+  let thead = document.createElement('thead');
+  let tr = document.createElement('tr');
+
+  let th = document.createElement('th');
+  th.appendChild(document.createTextNode("Item name"));
+  tr.appendChild(th);
+  th = null;
+
+  th = document.createElement('th');
+  th.appendChild(document.createTextNode("Price"));
+  tr.appendChild(th);
+
+  thead.appendChild(tr);
+  tbl.appendChild(thead);
+
+  let tbody = document.createElement('tbody');
+
+  for(let i = 0; i < tableContent.length; i++) {
+    tr = tbody.insertRow();
+    for(let j = 0; j < 2; j++) {
+      if (j === 0) {
+        let td = tr.insertCell();
+        let a = document.createElement('a');
+        a.href = tableContent[i].link.trim();
+        a.target = "_blank";
+        a.appendChild(document.createTextNode(tableContent[i].title.trim()));
+        td.appendChild(a);
+      }
+      if (j === 1) {
+        let td = tr.insertCell();
+        td.appendChild(document.createTextNode(tableContent[i].price.trim()));
+      }
+    }
+  }
+
+  tbl.appendChild(tbody);
+
+  return tbl;
+}
